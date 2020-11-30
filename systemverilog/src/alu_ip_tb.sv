49dceff7396838b30d675fba488adcb2b9c10c49
aba <abarajithan07@gmail.com>
2020-11-30 20:12:03 +0530

    Change always to forever in alu_ip

--- systemverilog/src/alu_ip_tb.sv
+++ systemverilog/src/alu_ip_tb.sv
@@ -16,24 +16,18 @@ module alu_ip_tb();
   alu_ip    dut (.*);
 
   int status, file_out, file_in;
-
-  // paths W.R.T to "project.sim/sim_1/behav/xsim" folder
+  /* paths W.R.T to "project.sim/sim_1/behav/xsim" folder */
   string path_in  ="../../../../../../data/data_input.txt";
   string path_out ="../../../../../../data/data_output.txt";
+// string path_in  ="U:/aba/Desktop_Win/missing_semester/systemverilog/data/data_input.txt";
+// string path_out ="U:/aba/Desktop_Win/missing_semester/systemverilog/data/data_output.txt";
+
   initial begin
     file_in  = $fopen(path_in, "r");
     file_out = $fopen(path_out, "w");
-  end
 
-  always @ (posedge clk) begin
-    #(CLK_PERIOD/2);
-    $fdisplay(file_out, "%d", C);
-  end
-
-  initial begin
     while(!$feof(file_in)) begin
-      @(posedge clk);
-      #1;
+      @(posedge clk); #1;
       status = $fscanf(file_in,"%d\r", A  );
       status = $fscanf(file_in,"%d\r", B  );
       status = $fscanf(file_in,"%d\r", sel);
@@ -42,5 +36,12 @@ module alu_ip_tb();
     #(CLK_PERIOD*3);
     $fclose(file_in);
     $fclose(file_out);
+  end
+
+  initial begin
+    forever begin
+      @(posedge clk); #1;
+      $fdisplay(file_out, "%d", C);
+    end
   end
 endmodule
