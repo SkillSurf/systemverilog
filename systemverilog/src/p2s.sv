3fe8403b6d03f2e381a687a3ebd0041c43617640
aba <abarajithan07@gmail.com>
2022-08-30 10:01:28 +0530

    Update p2s with rstn, better formatting

--- systemverilog/src/p2s.sv
+++ systemverilog/src/p2s.sv
@@ -1,29 +1,27 @@
-module p2s #(parameter N = 8)
+module p2s #(N = 8)
 (
-  input  logic clk,
+  input  logic         clk, 
+                       rstn, 
+                       s_ready,
+                       p_valid, 
   input  logic [N-1:0] p_data,
-  input  logic p_valid,
-  output logic p_ready,
-  output logic s_data,
-  output logic s_valid,
-  input  logic s_ready
+  output logic         p_ready, 
+                       s_data, 
+                       s_valid
 );
-  typedef enum logic [0:0] {
+  typedef enum logic {
     RX = 1'b0,
     TX = 1'b1
-  } states_t;
+  } e_states;
 
   localparam N_BITS = $clog2(N);
 
-  // Initial values
-  // when FPGA powers up
-
-  logic [N_BITS-1:0] count = 0;
-  states_t state = RX;
+  logic [N_BITS-1:0] count; // = 0; // Initial values when FPGA powers up
+  e_states state;  //= RX;
 
   // NEXT STATE DECODER LOGIC
 
-  states_t next_state;
+  e_states next_state;
   always_comb begin
     unique case (state)
       RX: begin
@@ -42,9 +40,9 @@ module p2s #(parameter N = 8)
 
   // STATE SEQUENCER LOGIC
 
-  always_ff @(posedge clk) begin
-    state <= next_state;
-  end
+  always_ff @(posedge clk or negedge rstn)
+    if (!rstn) state <= RX;
+    else       state <= next_state;
 
   // OUTPUT DECODE LOGIC
 
@@ -54,74 +52,71 @@ module p2s #(parameter N = 8)
   assign p_ready = (state == RX);
   assign s_valid = (state == TX);
 
-  always_ff @(posedge clk) begin
+  always_ff @(posedge clk or negedge rstn)
+  if (!rstn) count     <= '0;
+  else 
     unique case (state)
-      RX: shift_reg <= p_data;
-      TX: begin
-            if (s_ready) begin
-              // shift_reg <= shift_reg >> 1;
-              shift_reg <= {1'd0, shift_reg[N-1:1]};
-              count     <= count + 1'd1;
-            end
-            else begin
-              shift_reg <= shift_reg;
-              count     <= count;
-            end
+      RX:    shift_reg <= p_data;
+      TX: if (s_ready) begin
+             shift_reg <= shift_reg >> 1;
+             count     <= count + 1'd1;
+          end else begin
+             shift_reg <= shift_reg;
+             count     <= count;
           end
     endcase
-  end
 endmodule
 
 module p2s_tb();
-    timeunit       1ns;
-    timeprecision  1ps;
+  timeunit       1ns;
+  timeprecision  1ps;
 
-    logic clk = 0;
-    localparam CLK_PERIOD = 10;
-    initial begin
-      forever begin
-        #(CLK_PERIOD/2);
-        clk <= ~clk;
-      end
-    end
+  logic clk = 0, rstn = 0;
+  localparam CLK_PERIOD = 10;
+  initial forever 
+    #(CLK_PERIOD/2) 
+    clk <= ~clk;
 
-    parameter N = 8;
-    logic [N-1:0] p_data;
-    logic         p_valid;
-    logic         p_ready;
-    logic         s_data;
-    logic         s_valid;
-    logic         s_ready;
+  parameter N = 8;
+  logic [N-1:0] p_data;
+  logic p_valid, p_ready, 
+        s_data, s_valid, 
+        s_ready;
 
-    p2s #(.N(N)) dut (.*);
+  p2s #(.N(N)) dut (.*);
 
-    initial begin
+  initial begin
+    @(posedge clk);
+    #1  rstn <= 1;
+
+    @(posedge clk);
+    #1  p_data  <= 8'd7; 
+        p_valid <= 0; 
+        s_ready <= 1;
+    
+    repeat(10) 
       @(posedge clk);
-      p_data  <= 8'd7;
-      p_valid <= 0;
-      s_ready <= 1;
+    
+    @(posedge clk);
+    #1  p_data  <= 8'd62; 
+        p_valid <= 1;
 
-      #(CLK_PERIOD*2);
-      @(posedge clk);
-      p_data  <= 8'd62;
-      p_valid <= 1;
+    @(posedge clk);
+    #1  p_valid <= 0;
 
+    repeat(10) 
       @(posedge clk);
-      p_valid <= 0;
+    #1  p_data  <= 8'd52; 
+        p_valid <= 1;
 
-      #(CLK_PERIOD*10);
-      @(posedge clk);
-      p_data  <= 8'd52;
-      p_valid <= 1;
+    @(posedge clk);
+    #1  p_valid <= 0; 
 
-      @(posedge clk);
-      p_valid <= 0;
+    @(posedge clk);
+    #1  s_ready <= 0;
 
+    repeat(3) 
       @(posedge clk);
-      s_ready <= 0;
-
-      #(CLK_PERIOD*3);
-      @(posedge clk);
-      s_ready <= 1;
-    end
+    s_ready <= 1;
+  end
 endmodule
