6b67c59033d375da7075d1a86f310be5b2d15f52
Aba <abarajithan-7@gmail.com>
2023-01-16 02:32:53 -0800

    Fix name in alu

--- sv/3_alu_tb.sv
+++ sv/3_alu_tb.sv
@@ -17,9 +17,9 @@ module alu_tb;
   timeunit 1ns/1ps;
 
   localparam WIDTH = 8;
-  logic        [2:0] sel;
+  logic        [2:0]       alu_sel;
   logic signed [WIDTH-1:0] bus_a,bus_b,alu_out;
-  logic Z;
+  logic                    zero, negative;
 
   alu #(.WIDTH(WIDTH)) dut (.*);
 
@@ -31,16 +31,16 @@ module alu_tb;
 
     repeat(5) begin
       #10
-      bus_a <= A_r.num; bus_b <= B_r.num; sel <= sel_r.num;
+      bus_a <= A_r.num; bus_b <= B_r.num; alu_sel <= sel_r.num;
       
       A_r.randomize();
       B_r.randomize();
       sel_r.randomize();
     end
 
-    #30 bus_a <= 8'd5;  bus_b <=  8'd10; sel <= 3'b000;
-    #10 bus_a <= 8'd30; bus_b <= -8'd10; sel <= 3'b001;
-    #10 bus_a <= 8'd5;  bus_b <=  8'd10; sel <= 3'b010;
-    #10 bus_a <= 8'd51; bus_b <=  8'd17; sel <= 3'b011;
+    #30 bus_a <= 8'd5;  bus_b <=  8'd10; alu_sel <= 3'b000;
+    #10 bus_a <= 8'd30; bus_b <= -8'd10; alu_sel <= 3'b001;
+    #10 bus_a <= 8'd5;  bus_b <=  8'd10; alu_sel <= 3'b010;
+    #10 bus_a <= 8'd51; bus_b <=  8'd17; alu_sel <= 3'b011;
   end
 endmodule
