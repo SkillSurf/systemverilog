fb122454f5e6a819dcc1958e269af972d184bef6
agnaneswaran <agnaneswaran@ieng6-ece-03.ucsd.edu>
2023-02-13 15:31:02 -0800

    Add run_dc.tcl, update fir_filter

---
+++ asic_flow/run_dc.tcl
@@ -0,0 +1,86 @@
+set top_module fir_filter
+set rtlPath "../../sv/"
+exec mkdir -p log
+
+# Target library
+set target_library /home/linux/ieng6/ee260bwi23/public/PDKdata/db/tcbn65gpluswc.db 
+set link_library $target_library
+
+#Compiler directives
+set compile_effort "low"
+set compile_no_new_cells_at_top_level false
+set hdlin_enable_vpp true
+set hdlin_auto_save_templates false
+set wire_load_mode enclosed
+set timing_use_enhanced_capacitance_modeling true
+set verilogout_single_bit false
+remove_design -all
+
+define_design_lib WORK -path .template
+
+# read RTL
+analyze -format sverilog -lib WORK [glob ${rtlPath}*_${top_module}.sv]
+
+elaborate $top_module -lib WORK -update
+current_design $top_module
+
+# Link Design
+link
+
+# Default SDC Constraints (can be an sdc file)
+set clock_cycle 1.6
+set io_delay 0.2 
+create_clock -name clk -period $clock_cycle [get_ports clk]
+set_input_delay -clock [get_clocks clk] -add_delay -max $io_delay [all_inputs]
+set_output_delay -clock [get_clocks clk] -add_delay -max $io_delay [all_outputs]
+# set_input_delay -clock [get_clocks clk] -add_delay -max $io_delay [get_ports {key[52]}]
+# read_sdc ${top_module}.sdc
+
+propagate_constraints
+current_design $top_module
+
+set_cost_priority {max_transition max_fanout max_delay max_capacitance}
+set_fix_multiple_port_nets -all -buffer_constants
+set_fix_hold [all_clocks]
+
+set_driving_cell -lib_cell BUFFD8 -pin Z [all_inputs]
+# set_load [get_attribute "$target_library/BUFFD8/A" fanout_load] [all_outputs]
+foreach_in_collection p [all_outputs] {
+	set_load 0.050 $p
+}
+
+#More compiler directives
+set compile_effort   "high"
+set_app_var ungroup_keep_original_design true
+set_register_merging [get_designs $top_module] false
+set compile_seqmap_propagate_constants false
+set compile_seqmap_propagate_high_effort false
+# More constraints and setup before compile
+foreach_in_collection design [ get_designs "*" ] {
+	current_design $design
+	#feedthrough / outputs / constants
+	set_fix_multiple_port_nets -all
+}
+current_design $top_module
+
+# Compile
+compile_ultra -no_autoungroup -timing_high_effort_script -exact_map
+
+# Write Out Design - Hierarchical
+current_design $top_module
+change_names -rules verilog -hierarchy
+write -format verilog -hier -output ${top_module}.out.v
+
+# Write Reports
+redirect "log/${top_module}_area.rep" {report_area}
+redirect -append "log/${top_module}_area.rep" {report_reference}
+redirect "log/${top_module}_power.rep" {report_power}
+redirect "log/${top_module}_timing.rep" {report_timing -path full -max_paths 100 -nets -transition_time -capacitance -significant_digits 3 -nosplit}
+
+
+set unmapped_designs [get_designs -filter "is_unmapped == true" $top_module]
+if {  [sizeof_collection $unmapped_designs] != 0 } {
+	echo "****************************************************"
+	echo "* ERROR!!!! Compile finished with unmapped logic.  *"
+	echo "****************************************************"
+}
--- sv/5_fir_filter.sv
+++ sv/5_fir_filter.sv
@@ -1,39 +1,34 @@
 `timescale 1ns/1ps
 
 module fir_filter #(
-  parameter  N = 5,
-             WIDTH_X = 8,
-             WIDTH_B = 3,
-  parameter logic [WIDTH_B-1:0] B [N+1] = '{default:0},
+  parameter  N = 5, W_X = 8, W_B = 3,
+  parameter logic signed [W_B-1:0] B [N+1] = {
+    8'd1, 8'd2, 8'd3, 8'd4, 8'd5, 8'd6},//'{default:0},
   
-  localparam WIDTH_M = WIDTH_X + WIDTH_B,
-             WIDTH_Y = WIDTH_M + N + 1
+  localparam W_M = W_X + W_B,
+             W_Y = W_M + N + 1
 )(
   input  clk, rstn,
-  input  logic signed [WIDTH_X-1:0] x,
-  output logic signed [WIDTH_Y-1:0] y
+  input  logic signed [W_X-1:0] x,
+  output logic signed [W_Y-1:0] y
 );
 
-  logic [WIDTH_X-1:0] z [N+1];
-  logic [WIDTH_M-1:0] m [N+1];
-  logic [WIDTH_Y-1:0] a [N+1];
+  genvar n;
+  logic signed [W_X-1:0] z [N+1];
+  logic signed [W_M-1:0] m [N+1];
+  logic signed [W_Y-1:0] a [N+1];
   
   assign z[0] = x;
-
-  always_ff @(posedge clk or negedge rstn)
-    for (int n=1; n <= N; n=n+1)
-      if (~rstn) z[n] <= 0;
+  
+  for (n=1; n<= N; n=n+1)
+    always_ff @(posedge clk or negedge rstn)
+      if (~rstn) z[n] <= '0;
       else       z[n] <= z[n-1];
 
-  always_comb
-    for (int n=0; n <= N; n=n+1)
-      m[n] = B[n] * z[n];
-
-  assign a[0] = m[0];
-  
-  always_comb
-    for (int n=1; n <= N; n=n+1)
-      a[n] = a[n-1] + m[n];
+  for (n=0; n <= N; n=n+1) begin
+    assign m[n] = B[n] * z[n];
+    assign a[n] = n==0 ? m[0] : a[n-1] + m[n];
+  end
 
   assign y = a[N];
 
