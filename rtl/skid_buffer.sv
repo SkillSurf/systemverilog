2739913915ec0939a1cc13fea8020a9f4efeefdc
Aba <abarajithan-7@gmail.com>
2024-05-18 15:49:18 -0700

    Simplify skid buffer, make it full performance

--- rtl/skid_buffer.sv
+++ rtl/skid_buffer.sv
@@ -1,40 +1,38 @@
-module skid_buffer #( parameter WIDTH = 8)(
+module skid_buffer #(parameter WIDTH = 8)(
   input  logic clk, rstn, s_valid, m_ready,
   input  logic [WIDTH-1:0] s_data,
   output logic [WIDTH-1:0] m_data,
   output logic m_valid, s_ready
 );
-  enum {FULL, EMPTY} state, state_next;
+  enum {EMPTY, PARTIAL, FULL} state, state_next;
   always_comb begin
     state_next = state;
-    case (state)
-      EMPTY : if(!m_ready && s_ready && s_valid) state_next = FULL;
-      FULL  : if(m_ready)                        state_next = EMPTY;
+    unique case (state)
+      EMPTY  : if      ( s_valid)             state_next = PARTIAL;
+      PARTIAL: if      (!m_ready &&  s_valid) state_next = FULL;
+               else if ( m_ready && !s_valid) state_next = EMPTY;
+      FULL   : if      ( m_ready)             state_next = PARTIAL;
     endcase
   end
-  always_ff @(posedge clk)
-    if      (!rstn)              state <= EMPTY;
-    else if (m_ready || s_ready) state <= state_next;
+  always @(posedge clk)
+    if (!rstn) state <= EMPTY;
+    else       state <= state_next;
 
-  logic b_valid;
-  logic [WIDTH-1:0] b_data;
-  wire  [WIDTH-1:0] m_data_next  = (state      == FULL) ? b_data  : s_data;
-  wire              m_valid_next = (state      == FULL) ? b_valid : s_valid;
-  wire              buffer_en    = (state_next == FULL) && (state==EMPTY);
-  wire              m_en         = m_valid_next & m_ready;
+  logic [WIDTH-1:0] buffer;
+  always @(posedge clk)
+    if (!rstn) {m_valid, s_ready, buffer, m_data} <= 0;
+    else begin
+      
+      m_valid <= state_next != EMPTY;
+      s_ready <= state_next != FULL;
 
-  always_ff @(posedge clk)
-    if (!rstn) begin
-      s_ready <= 1;
-      {m_valid, b_valid} <= '0;
-    end else begin
-      s_ready <= state_next == EMPTY;
-      if (buffer_en) b_valid <= s_valid;      
-      if (m_ready  ) m_valid <= m_valid_next;
+      if (state == PARTIAL && state_next == FULL)
+        buffer <= s_data;
+
+      unique case (state)
+        EMPTY  : if (state_next == PARTIAL) m_data <= s_data;
+        PARTIAL: if (m_ready && s_valid)    m_data <= s_data;
+        FULL   : if (m_ready)               m_data <= buffer;
+      endcase
     end
-    
-  always_ff @(posedge clk) begin
-    if (m_en)                 m_data <= m_data_next;
-    if (buffer_en && s_valid) b_data <= s_data;
-  end
 endmodule
