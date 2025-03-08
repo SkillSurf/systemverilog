0148c1bdc4e0f6844dc5206fa78129e6f280304a
SupunDK <supun2110@gmail.com>
2025-03-09 01:55:24 +0530

    add PG routing commands issued in the GUI

--- asic_flow/scripts/icc2/icc2.tcl
+++ asic_flow/scripts/icc2/icc2.tcl
@@ -9,7 +9,7 @@
 #  MODIFY as required
 
 # set the working directory path
-set working_dir /evprj156/projects 
+set working_dir /evprj156/projects
 
 #/* Top-level Module                                  */
 set top_module full_adder
@@ -57,7 +57,7 @@ initialize_floorplan -core_utilization 0.1 -core_offset {5}
 #  Power Rings
 # -----------------------------------------
 
-connect_pg_net -automatic -all_blocks
+connect_pg_net -automatic 
 
 create_pg_ring_pattern ring_pattern -horizontal_layer M1 \
    -horizontal_width {1.5} -horizontal_spacing {0.5} \
@@ -68,10 +68,10 @@ set_pg_strategy core_ring \
 compile_pg -strategies core_ring
 
 # This will need to be done MANUALLY
-create_pg_mesh_pattern mesh_pattern -layers { {{horizontal_layer: M1} {width: 0.75} {pitch: 15} {spacing: interleaving}}  {{vertical_layer: M2} {width: 0.84} {pitch: 33.6} {spacing: interleaving}} }
-set_pg_strategy mesh_strategy -polygon {{1.000 4.880} {16.144 11.990}} -pattern {{pattern: mesh_pattern}{nets: {VDD VSS}}} -blockage {macros: all}
+create_pg_mesh_pattern mesh_pattern -layers { {{horizontal_layer: M1} {width: 0.75} {pitch: 30} {spacing: interleaving}}  {{vertical_layer: M2} {width: 0.84} {pitch: 33.6} {spacing: interleaving}} }
+set_pg_strategy mesh_strategy -polygon {{1.000 4.759} {21.312 16.965}} -pattern {{pattern: mesh_pattern}{nets: {VDD VSS}}} -blockage {macros: all}
 create_pg_std_cell_conn_pattern std_cell_pattern
-set_pg_strategy std_cell_strategy -polygon {{1.000 4.880} {16.144 11.990}} -pattern {{pattern: std_cell_pattern}{nets: {VDD VSS}}}
+set_pg_strategy std_cell_strategy -polygon {{1.000 4.759} {21.312 16.965}} -pattern {{pattern: std_cell_pattern}{nets: {VDD VSS}}}
 compile_pg -ignore_via_drc
 
 #------------------------------------------
